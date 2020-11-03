f111473e167f25829e137ad411fa27398f506b08
mihaidld <mihaidoldur@gmail.com>
2020-11-03 15:52:46 +0100

    added chakra components

--- src/layouts/EthersTab.js
+++ src/layouts/EthersTab.js
@@ -1,62 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { Heading, Text } from '@chakra-ui/core'
-import { ethers } from 'ethers'
+import React, { useState, useEffect } from "react";
+import {
+  Heading,
+  Badge,
+  Box,
+  Text,
+  VStack,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/core";
+import { ethers } from "ethers";
 
 function EthersTab() {
-  const [isEtherem, setIsEtherem] = useState(false)
-  const [isEnable, setIsEnable] = useState(false)
-  const [account, setAccount] = useState('0x0')
-  const [network, setNetwork] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [isEtherem, setIsEtherem] = useState(false);
+  const [isEnable, setIsEnable] = useState(false);
+  const [account, setAccount] = useState("0x0");
+  const [network, setNetwork] = useState(null);
+  const [balance, setBalance] = useState(0);
 
   // check if ethereum is injected
   useEffect(() => {
-    if (typeof window.ethereum !== 'undefined') {
-      setIsEtherem(true)
-    } else setIsEtherem(false)
-  }, [])
+    if (typeof window.ethereum !== "undefined") {
+      setIsEtherem(true);
+    } else setIsEtherem(false);
+  }, []);
 
   // connect metamask to app
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       try {
         const accounts = await window.ethereum.request({
-          method: 'eth_requestAccounts',
-        })
-        const account = accounts[0]
-        setIsEnable(true)
-        setAccount(account)
+          method: "eth_requestAccounts",
+        });
+        const account = accounts[0];
+        setIsEnable(true);
+        setAccount(account);
       } catch (e) {
-        setIsEnable(false)
+        setIsEnable(false);
       }
-    })()
-  }, [isEtherem])
+    })();
+  }, [isEtherem]);
 
   useEffect(() => {
-    ;(async () => {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const network = await provider.getNetwork()
-      const _balance = await provider.getBalance(account)
-      const balance = ethers.utils.formatEther(_balance)
-      setNetwork(network)
-      setBalance(balance)
-    })()
-  }, [isEnable, account])
+    (async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const network = await provider.getNetwork();
+      const _balance = await provider.getBalance(account);
+      const balance = ethers.utils.formatEther(_balance);
+      setNetwork(network);
+      setBalance(balance);
+    })();
+  }, [isEnable, account]);
 
   return (
-    <>
-      <Heading>Web3 with ethers.js</Heading>
-      <Text>Metamask status: {isEnable ? 'connected' : 'disconnect'}</Text>
+    <VStack>
+      <Heading mb={10}>Web3 with ethers.js</Heading>
+      <Box mb={5}>
+        Metamask status:{" "}
+        {isEnable ? (
+          <Badge colorScheme="green">connected</Badge>
+        ) : (
+          <Badge colorScheme="red">disconnect</Badge>
+        )}
+      </Box>
       {network !== null && (
-        <>
-          <Text>Account: {account}</Text>
-          <Text>Network name: {network.name}</Text>
-          <Text>Network id: {network.chainId}</Text>
-          <Text>Balance: {balance}</Text>
-        </>
+        <UnorderedList>
+          <ListItem mb={3}>
+            Account: <Text as="b">{account}</Text>
+          </ListItem>
+          <ListItem mb={3}>
+            Network name: <Text as="b">{network.name}</Text>
+          </ListItem>
+          <ListItem mb={3}>
+            Network id: <Text as="b">{network.chainId}</Text>
+          </ListItem>
+          <ListItem mb={3}>
+            Balance: <Text as="b">{balance}</Text>
+          </ListItem>
+        </UnorderedList>
       )}
-    </>
-  )
+    </VStack>
+  );
 }
 
-export default EthersTab
+export default EthersTab;
